ba26f5c7396e45b1c85f497216548f569d1e92ff
JohnMFG <jmazeika78@gmail.com>
2023-05-15 14:53:37 +0300

    re 2

---
+++ React/project_02/src/MainContent.js
@@ -0,0 +1,17 @@
+function MainContent() {
+    return (
+        <div>
+            <h1>My awesome website in React</h1>
+            <h3>Reasons I love React:</h3>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, includinf mobile apps</li>
+            </ul>
+        </div>
+    )
+  }
+
+  export default MainContent;
